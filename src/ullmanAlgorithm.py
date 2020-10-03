c4acdcc8b712709396523ecc91c67050e4ff6daa
Veronika Wu <veronikawu23@gmail.com>
2020-10-03 17:04:12 +0200

    Initializing rotationsmatrix M with DEG-Kriterium

--- src/ullmanAlgorithm.py
+++ src/ullmanAlgorithm.py
@@ -10,22 +10,43 @@ def perform_ullman_algorithm(matchingGraph, originalGraph):
 def init(matchingGraph, orignialGraph):
     A = create_adj_matrix(matchingGraph)
     B = create_adj_matrix(orignialGraph)
-    H = create_vector(matchingGraph)
     F = create_vector(orignialGraph)
-    create_rotation_matrix()
-    return
+    H = create_vector(matchingGraph)
+    M = create_rotation_matrix(matchingGraph, orignialGraph, A, B)
+    return M, F, H
 
 
-def create_adj_matrix(graph):
-    G = np.array(nx.to_numpy_matrix(graph, dtype=int))
+def create_adj_matrix(Graph):
+    G = np.array(nx.to_numpy_matrix(Graph, dtype=int))
     return G
 
 
-def create_rotation_matrix():
-    return
+def create_rotation_matrix(*args):
+    """
+    :param args: (1) matching Graph, (2) original Graph, (3) adj. matrix matching Graph, (4) adj. matrix original Graph
+    :return: M0
+    """
+    A = args[2]
+    B = args[3]
+
+    M = np.zeros(shape=(len(A), len(B)), dtype=int)
+
+    degA = A.sum(axis=0)
+    degB = B.sum(axis=0)
+
+    for i in range(len(A)):
+        for j in range(len(B)):
+            if degB[j] >= degA[i]:
+                M[i][j] = 1
+
+    return M
 
 
 def create_vector(graph):
+    """
+    :param graph:
+    :return: zerovector with length = number of nodes of graph
+    """
     x = graph.number_of_nodes()
     F = np.zeros(x, dtype=int)
     return F
--- tests/test_ullmanAlgorithm.py
+++ tests/test_ullmanAlgorithm.py
@@ -1,13 +1,12 @@
 import unittest
 import networkx as nx
+import numpy as np
 from ullmanAlgorithm import create_adj_matrix
 from ullmanAlgorithm import create_vector
+from ullmanAlgorithm import create_rotation_matrix
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
     def test_create_adj_matrix(self):
         G = nx.Graph()
         G.add_nodes_from([1, 2])
@@ -25,12 +24,69 @@ class MyTestCase(unittest.TestCase):
         G.add_edge(1, 2)
 
         F = create_vector(G)
-        assert(len(F) == 2)
-        assert(F[0] == 0)
-        assert(F[1] == 0)
+        assert (len(F) == 2)
+        assert (F[0] == 0)
+        assert (F[1] == 0)
 
         F[0] = 1
-        assert(F[0] == 1)
+        assert (F[0] == 1)
+
+    def test_create_rotation_matrix(self):
+        G1 = nx.Graph()
+        G1.add_node(1, chem="H")
+        G1.add_node(2, chem="O")
+        G1.add_node(3, chem="H")
+        G1.add_edge(1, 2)
+        G1.add_edge(2, 3)
+
+        G2 = nx.Graph()
+        G2.add_node(1, chem="H")
+        G2.add_node(2, chem="O")
+        G2.add_node(3, chem="C")
+        G2.add_node(4, chem="H")
+        G2.add_edge(1, 2)
+        G2.add_edge(2, 3)
+        G2.add_edge(2, 4)
+
+        G3 = nx.Graph()
+        G3.add_node(1, chem="H")
+        G3.add_node(2, chem="H")
+        G3.add_node(3, chem="H")
+        G3.add_node(4, chem="H")
+        G3.add_edge(1, 2)
+        G3.add_edge(2, 3)
+        G3.add_edge(2, 4)
+
+        # x = nx.get_node_attributes(G1, "chem")
+        # print(x[1])
+        #
+        # print(nx.get_node_attributes(G1, "chem"))
+        # print(nx.get_node_attributes(G2, "chem"))
+        # print(nx.get_node_attributes(G3, "chem"))
+        #
+        # print(G1.nodes.data())
+        # print(G1.graph)
+
+        A = create_adj_matrix(G1)
+        B1 = create_adj_matrix(G2)
+        B2 = create_adj_matrix(G3)
+
+        M = create_rotation_matrix(G1, G2, A, B1)
+
+        # assertions only for DEG-Kriterium!
+        assert (M[0][0] == 1)
+        assert (M[0][1] == 1)
+        assert (M[0][2] == 1)
+        assert (M[0][3] == 1)
+        assert (M[1][0] == 0)
+        assert (M[1][1] == 1)
+        assert (M[1][2] == 0)
+        assert (M[1][3] == 0)
+        assert (M[2][0] == 1)
+        assert (M[2][1] == 1)
+        assert (M[2][2] == 1)
+        assert (M[2][3] == 1)
+
 
 
 
