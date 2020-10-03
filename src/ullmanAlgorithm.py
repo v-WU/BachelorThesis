785f06b0238606cab77ff333f001ca789d372650
Veronika Wu <veronikawu23@gmail.com>
2020-10-03 08:45:24 +0200

    Creating outline of init()

---
+++ src/ullmanAlgorithm.py
@@ -0,0 +1,32 @@
+import networkx as nx
+import numpy as np
+
+
+def perform_ullman_algorithm(matchingGraph, originalGraph):
+    init(matchingGraph, originalGraph)
+    return
+
+
+def init(matchingGraph, orignialGraph):
+    create_adj_matrix(matchingGraph)
+    create_adj_matrix(orignialGraph)
+    create_rotation_matrix()
+    create_vector_f()
+    create_vector_h()
+    return
+
+
+def create_adj_matrix(graph):
+    return
+
+
+def create_rotation_matrix():
+    return
+
+
+def create_vector_f():
+    return
+
+
+def create_vector_h():
+    return
