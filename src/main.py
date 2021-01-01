5470c9ada465a80c5af26ff9ce883b261252b103
Veronika Wu <veronikawu23@gmail.com>
2021-01-01 12:53:58 +0100

    preparing main for next run

--- src/main.py
+++ src/main.py
@@ -12,6 +12,12 @@ from src.runHelper import create_txt_files
 from plot_utility import create_table, create_diagram_for_bsc, create_df_for_bsc, create_df_for_F, create_diagram_for_F, \
     create_df_for_F_2
 
+
+# TODO delete all bsc .jpg and redo the bsc diagrams
+
+# TODO for no_pruning_cost_1.6_dist_0.9 all sets
+# pruning = ["no_pruning"]
+# costs = [1.6]
 pruning = ["pruning", "no_pruning"]
 costs = [0.2, 0.3, 0.4, 0.6, 0.9, 1.6]
 set_types = ["train", "validation", "test"]
@@ -22,7 +28,6 @@ set_of_labels = ["A", "E", "F", "H", "I", "K", "L", "M", "N", "T", "V", "W", "X"
 
 dict = {"train": train_graphs, "test": test_graphs, "validation": validation_graphs}
 
-# TODO for no_pruning_cost_1.6_dist_0.9 all sets
 for pr in pruning:
     for cost in costs:
         mg_path = "Data/vero_folder_letter/matching_graphs_{}_costs_{}".format(pr, cost)
@@ -30,22 +35,24 @@ for pr in pruning:
         # matching_graphs = matching_graphs[:5]
         for set_type in set_types:
             path = "letter_results/{}_cost_{}_dist_0.9_{}".format(pr, cost, set_type)
-            create_txt_files(path, matching_graphs, dict[set_type])
+            create_txt_files(path, matching_graphs, dict[set_type])  # comment when doing only the bsc diagrams
             df, names_OG, names_MG = create_table(path)
-            path2 = create_abs_path(path)
-            df.to_csv(path2 + "/isomorphism_table.csv")
-            df.to_excel(path2 + "/isomorphism_table.xlsx")
+            path2 = create_abs_path(path)  # comment when doing only the bsc diagrams
+            df.to_csv(path2 + "/isomorphism_table.csv")  # comment when doing only the bsc diagrams
+            df.to_excel(path2 + "/isomorphism_table.xlsx")  # comment when doing only the bsc diagrams
 
             for cls in set_of_labels:
                 subdf = create_df_for_bsc(df, names_OG, names_MG, cls)
-                subdf.to_csv(path2 + "/diagrams/Mgs_" + cls + ".csv")
-                subdf.to_excel(path2 + "/diagrams/Mgs_" + cls + ".xlsx")
+                subdf.to_csv(path2 + "/diagrams/Mgs_" + cls + ".csv")  # comment when doing only the bsc diagrams
+                subdf.to_excel(path2 + "/diagrams/Mgs_" + cls + ".xlsx")  # comment when doing only the bsc diagrams
                 test = pd.read_csv(path2 + "/diagrams/Mgs_" + cls + ".csv")
                 create_diagram_for_bsc(test, cls, path)  # get saved in folder
 
+                # comment when doing only the bsc diagrams
                 subdf2 = create_df_for_F(df, names_OG, names_MG, cls, set_of_labels)
                 subdf2.to_csv(path2 + "/diagrams_for_F/Mgs_Class_" + cls + ".csv")
                 subdf2.to_excel(path2 + "/diagrams_for_F/Mgs_Class_" + cls + ".xlsx")
                 create_diagram_for_F(subdf2, cls, path)  # get saved in folder
 
+            # comment when doing only the bsc diagrams
             create_df_for_F_2(df, orig_names=names_OG, mg_names=names_MG, set_of_classes=set_of_labels, path=path)  # gets saved in folder
